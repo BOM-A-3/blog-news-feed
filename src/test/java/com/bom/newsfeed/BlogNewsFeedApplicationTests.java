0eab0d023345525740a271f53b932fa4b32c6410
MinJung Kim <kminss3711@gmail.com>
2023-11-21 23:37:31 +0900

    initial commit

---
+++ src/test/java/com/bom/newsfeed/BlogNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bom.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogNewsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

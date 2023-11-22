c9e9f9b1d22a443a197d1bd431a888d0a5a2c2de
MinJung Kim <kminss3711@gmail.com>
2023-11-22 16:35:00 +0900

    Fix: 카테고리 도메인 구현

---
+++ src/main/java/com/bom/newsfeed/domain/category/constant/CategoryType.java
@@ -0,0 +1,29 @@
+package com.bom.newsfeed.domain.category.constant;
+
+import java.util.Arrays;
+
+public enum CategoryType {
+	LIFE("일상"),
+	IT("IT"),
+	TRAVEL("여행"),
+	COOK("요리"),
+	ETC("기타");
+
+	private final String ko;
+
+	CategoryType(String ko) {
+		this.ko = ko;
+	}
+
+	public String getKo() {
+		return ko;
+	}
+
+	public static CategoryType findByName(String categoryType) {
+		return Arrays.stream(CategoryType.values())
+			.filter(type ->
+				type.name().equals(categoryType.toUpperCase()))
+			.findFirst()
+			.orElse(CategoryType.ETC);
+	}
+}

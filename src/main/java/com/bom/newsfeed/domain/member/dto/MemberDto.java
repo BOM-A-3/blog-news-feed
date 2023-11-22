1fec0db6c58bfd38fc942c47be533f7ea91c72fb
MinJung Kim <kminss3711@gmail.com>
2023-11-22 18:36:07 +0900

    Refactor: MemberDTo에서 Member 엔티티 변환 메소드 추가

--- src/main/java/com/bom/newsfeed/domain/member/dto/MemberDto.java
+++ src/main/java/com/bom/newsfeed/domain/member/dto/MemberDto.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 
 @Getter
 public class MemberDto {
+	private final Long id;
 	private final String username;
 	private final String password;
 	private final String nickname;
 	private final MemberRole role;
 
-	public MemberDto(String username, String password, String nickname, MemberRole role) {
+	public MemberDto(Long id, String username, String password, String nickname, MemberRole role) {
+		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.nickname = nickname;
@@ -21,10 +23,21 @@ public class MemberDto {
 
 	public static MemberDto from (Member entity) {
 		return new MemberDto(
+			entity.getId(),
 			entity.getUsername(),
 			entity.getPassword(),
 			entity.getNickname(),
 			entity.getRole()
+		);
+	}
+
+	public Member toEntity() {
+		return Member.of(
+			id,
+			username,
+			password,
+			nickname,
+			role
 		);
 	}
 }

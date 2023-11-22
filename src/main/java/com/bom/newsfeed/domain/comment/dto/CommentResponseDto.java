cbe6de66bbd4e20569ccf7350e60c89c7721a48e
kkamjjing2 <kkamjjing2@gmail.com>
2023-11-22 13:50:32 +0900

    feat : CommentResponseDto

--- src/main/java/com/bom/newsfeed/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/bom/newsfeed/domain/comment/dto/CommentResponseDto.java
@@ -8,4 +8,18 @@ import lombok.Getter;
 
 @Getter
 public class CommentResponseDto {
+
+	private final Long id;
+	private final String username;
+	private final String comment;
+	private final LocalDateTime createdDateTime;
+	private final LocalDateTime modifiedDateTime;
+
+	public CommentResponseDto (Comment comment) {
+		this.id = comment.getId();
+		this.username = comment.getMember().getUsername();
+		this.comment = comment.getComment();
+		this.createdDateTime = comment.getCreatedDateTime();
+		this.modifiedDateTime = comment.getModifiedDateTime();
+	}
 }

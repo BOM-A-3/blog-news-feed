ef2115936289c957930c072ee8270f4024758071
MinJung Kim <kminss3711@gmail.com>
2023-11-24 20:05:00 +0900

    Refactor: 공통 정상/예외 응답 리팩토링
    정상 응답 포맷이 추가되면서
    전체적으로 코드를 리팩토링함

--- src/main/java/com/bom/newsfeed/global/common/dto/ErrorResponse.java
+++ src/main/java/com/bom/newsfeed/global/common/dto/ErrorResponse.java
@@ -1,38 +1,18 @@
 package com.bom.newsfeed.global.common.dto;
 
-import java.time.LocalDateTime;
-
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class ErrorResponse {
-    @Schema(description = "발생일")
-    private final LocalDateTime timestamp = LocalDateTime.now();
-    @Schema(
-            description = "상태코드",
-            example = "500"
-    )
-    private final int status;
-    @Schema(
-            description = "에러명",
-            example = "INTERNAL_SERVER_ERROR"
-    )
-    private final String name;
-    @Schema(
-            description = "에러 메시지",
-            example = "내부 서버 에러입니다."
-    )
-    private final String message;
-    @Schema(description = "데이터", nullable = true)
-    private final Object data;
+public class ErrorResponse<T> extends DefaultRes<T>{
+
 
     @Builder
-    protected ErrorResponse(int status, String name, String message, Object data) {
-        this.status = status;
-        this.name = name;
-        this.message = message;
-        this.data = data;
+    public ErrorResponse(int statusCode, String message, T data) {
+        super(
+            statusCode,
+            message,
+            data
+        );
     }
 }

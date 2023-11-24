878e4d8788df09812ce35ff052c4dcb32aaa83a6
MinJung Kim <kminss3711@gmail.com>
2023-11-24 20:07:52 +0900

    Refactor: 응답/예외 포맷 변경
    포맷이 변경되어 커스텀 예외, 컨트롤러 응답 모두 변경

--- src/main/java/com/bom/newsfeed/global/exception/AlreadyExistFollowingException.java
+++ src/main/java/com/bom/newsfeed/global/exception/AlreadyExistFollowingException.java
@@ -1,5 +1,7 @@
 package com.bom.newsfeed.global.exception;
 
+import com.bom.newsfeed.global.common.constant.ErrorCode;
+
 public class AlreadyExistFollowingException extends ApiException{
 	public AlreadyExistFollowingException() {
 		super(ErrorCode.ALREADY_EXIST_FOLLOWING);
--- src/main/java/com/bom/newsfeed/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/bom/newsfeed/global/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.bom.newsfeed.global.filter;
 
-
-import static com.bom.newsfeed.global.exception.ErrorCode.*;
+import static com.bom.newsfeed.global.common.constant.ErrorCode.*;
 
 import java.io.IOException;
 
@@ -13,10 +12,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.bom.newsfeed.domain.member.constant.MemberRole;
 import com.bom.newsfeed.global.auth.TokenDto;
-import com.bom.newsfeed.global.security.UserDetailsImpl;
 import com.bom.newsfeed.global.common.dto.ErrorResponse;
 import com.bom.newsfeed.global.common.dto.LoginRequest;
-import com.bom.newsfeed.global.security.UserDetailsServiceImpl;
+import com.bom.newsfeed.global.security.UserDetailsImpl;
 import com.bom.newsfeed.global.util.JwtUtil;
 import com.bom.newsfeed.global.util.RedisUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,66 +29,67 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "로그인 인증")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    private final JwtUtil jwtUtil;
-    private final RedisUtil redisUtil;
-    private final ObjectMapper objectMapper;
+	private final JwtUtil jwtUtil;
+	private final RedisUtil redisUtil;
+	private final ObjectMapper objectMapper;
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil,  RedisUtil redisUtil, ObjectMapper objectMapper) {
-        this.jwtUtil = jwtUtil;
-        this.redisUtil = redisUtil;
-        this.objectMapper = objectMapper;
+	public JwtAuthenticationFilter(JwtUtil jwtUtil, RedisUtil redisUtil, ObjectMapper objectMapper) {
+		this.jwtUtil = jwtUtil;
+		this.redisUtil = redisUtil;
+		this.objectMapper = objectMapper;
 
-        setFilterProcessesUrl("/api/auth/login");
-    }
+		setFilterProcessesUrl("/api/auth/login");
+	}
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        try {
-            LoginRequest loginRequest = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
+		AuthenticationException {
+		try {
+			LoginRequest loginRequest = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
 
-            return getAuthenticationManager().authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginRequest.getUsername(),
-                            loginRequest.getPassword(),
-                            null
-                    )
-            );
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        }
-    }
+			return getAuthenticationManager().authenticate(
+				new UsernamePasswordAuthenticationToken(
+					loginRequest.getUsername(),
+					loginRequest.getPassword(),
+					null
+				)
+			);
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        log.info("로그인 인증 성공");
-        String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
-        MemberRole role = ((UserDetailsImpl) authentication.getPrincipal()).getMemberDto().getRole();
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+		Authentication authentication) throws IOException, ServletException {
+		log.info("로그인 인증 성공");
+		String username = ((UserDetailsImpl)authentication.getPrincipal()).getUsername();
+		MemberRole role = ((UserDetailsImpl)authentication.getPrincipal()).getMemberDto().getRole();
 
-        TokenDto tokenDto = jwtUtil.createToken(username, role);
-        jwtUtil.setTokenResponse(tokenDto, response);
-        redisUtil.saveKey("RefreshToken:" + username, 24 * 60, tokenDto.getRefreshToken());
-    }
+		TokenDto tokenDto = jwtUtil.createToken(username, role);
+		jwtUtil.setTokenResponse(tokenDto, response);
+		redisUtil.saveKey("RefreshToken:" + username, 24 * 60, tokenDto.getRefreshToken());
+	}
 
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException failed) throws IOException, ServletException {
+		log.info("로그인 인증 실패");
+		setResponseConfig(response);
+		objectMapper
+			.registerModule(new JavaTimeModule())
+			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+			.writeValue(response.getWriter(),
+				ErrorResponse.builder()
+					.statusCode(UNAUTHORIZED_MEMBER.getHttpStatus().value())
+					.message(UNAUTHORIZED_MEMBER.getDetail())
+					.build()
+			);
+	}
 
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        log.info("로그인 인증 실패");
-        setResponseConfig(response);
-        objectMapper
-                .registerModule(new JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .writeValue(response.getWriter(),
-                        ErrorResponse.builder()
-                                .status(UNAUTHORIZED_MEMBER.getHttpStatus().value())
-                                .name(UNAUTHORIZED_MEMBER.name())
-                                .message(UNAUTHORIZED_MEMBER.getDetail())
-                                .build()
-                );
-    }
-
-    private void setResponseConfig(HttpServletResponse response) {
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("utf-8");
-    }
+	private void setResponseConfig(HttpServletResponse response) {
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding("utf-8");
+	}
 }

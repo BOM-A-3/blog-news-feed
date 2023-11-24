a043c75ed35af9afa6ff6f621fbdae4b0214d77a
Min Jung Kim <73427028+Kminss@users.noreply.github.com>
2023-11-24 16:10:13 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,24 @@
 # deploy.sh
-
 #!/bin/bash
-BUILD_PATH=$(ls /home/ec2-user/app/blog-news-feed/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
-echo "> build 파일명: $JAR_NAME"
+REPOSITORY=/home/ec2-user/app/blog-news-feed
+cd $REPOSITORY
 
-DEPLOY_PATH=/home/ec2-user/app/blog-news-feed
+APP_NAME=BlogNewsFeedApp
+JAR_PATH=$(ls $REPOSITORY/build/libs/*.jar | grep -v 'plain')
 
-echo "> springboot-deploy.jar 교체"
-CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
-APPLICATION_JAR_NAME=blog-news-feed.jar
-APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
-
-ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
-
-echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 종료할 APP_NAME이 없음"
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $APPLICATION_JAR 배포"
-nohup java -jar -Dspring.profiles.active=prod $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 에 실행 권한 추가" 
+chmod +x $JAR_PATH
+
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &

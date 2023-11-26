bb66373b99b76e248ef3d90ad1b7d87f1025982a
MinJung Kim <kminss3711@gmail.com>
2023-11-27 00:34:44 +0900

    Refactor: 피드 응답 데이터 추가
    좋아요 수, 카테고리 추가함

--- src/main/java/com/bom/newsfeed/domain/post/dto/GetAllPostResponseDto.java
+++ src/main/java/com/bom/newsfeed/domain/post/dto/GetAllPostResponseDto.java
@@ -3,6 +3,7 @@ package com.bom.newsfeed.domain.post.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.bom.newsfeed.domain.category.constant.CategoryType;
 import com.bom.newsfeed.domain.post.entity.Post;
 
 import lombok.Getter;
@@ -18,8 +19,11 @@ public class GetAllPostResponseDto {
 
 	private final String memberName;
 
+	private final long likeCount;
+	private final String category;
 	private final List<GetPostFIleResponseDto> postFiles;
 	private final LocalDateTime createdDateTime;
+
 
 	public GetAllPostResponseDto(Post post)
 	{
@@ -27,6 +31,8 @@ public class GetAllPostResponseDto {
 		this.title = post.getTitle();
 		this.content = post.getContent();
 		this.memberName = post.getMember().getNickname();
+		this.likeCount = post.getLikes().size();
+		this.category = post.getCategory().getCategory().getKo();
 		this.postFiles = post.getPostFiles().stream().map(GetPostFIleResponseDto::new).toList();
 		this.createdDateTime = post.getCreatedDateTime();
 	}
